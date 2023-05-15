58aa2ffc14807bc9fafaef5ae587f54dfe91757f
最狂の戦士 <80885196+saikyounosennsi@users.noreply.github.com>
2023-05-15 19:14:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ const net = require("net");
 consote.log(test)
 // サーバーを設定
 const server = net.createServer((connection) => {
-  console.log("connected.");
-  connection.on("close", () => {
-    console.log("disconnected.");
-  });
-  connection.on("data", (data) => {
-    console.log(data.toString());
-    let parse_data = JSON.parse(data.toString());
-    let send_data = JSON.stringify({
-      result: parse_data.code * 4,
-      code: parse_data.code,
-    });
-    console.log(`send:${send_data}`);
-    connection.write(`${send_data}`);
-    connection.end();
-    console.log("testtest");
-  });
-  connection.on("error", (err) => {
-    console.error(err.message);
-  });
+console.log("connected.");
+connection.on("close", () => {
+console.log("disconnected.");
+});
+connection.on("data", (data) => {
+console.log(data.toString());
+let parse_data = JSON.parse(data.toString());
+let send_data = JSON.stringify({
+result: parse_data.code * 4,
+code: parse_data.code,
+});
+console.log(`send:${send_data}`);
+connection.write(`${send_data}`);
+connection.end();
+console.log("testtest");
+});
+connection.on("error", (err) => {
+console.error(err.message);
+});
 });
 
 // ソケットファイルを削除（存在するとlistenできない）
 try {
-  fs.unlinkSync("unix.sock");
+fs.unlinkSync("unix.sock");
 } catch (error) {}
 
 // UNIXドメインソケットでlistenする
